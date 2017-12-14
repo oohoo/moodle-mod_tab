c7e1c457a20e452eae02c9c09ada2bbd2f301ccc
patrick <patrick@ubuntu>
2017-12-14 13:22:51 -0500

    MAde compatible with Moodle 3.4

--- version.php
+++ version.php
@@ -13,9 +13,9 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
  * *************************************************************************
  * ************************************************************************ */
-$plugin->version = 2016053102;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version = 2017121400;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2010112400;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.3.0 (Build: 2016053100)';
+$plugin->release = '2.4.0 (Build: 2017121400)';
 $plugin->cron = 0;           // Period for cron to check this module (secs)
 $plugin->component = 'mod_tab';
