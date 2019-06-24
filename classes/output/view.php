5da349ef749200fac96ed19e90d9d1bac4487278
Patrick Thibaudeau <thibaud@yorku.ca>
2019-06-24 16:17:06 -0400

    Added header information in files

--- classes/output/view.php
+++ classes/output/view.php
@@ -1,10 +1,17 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * *************************************************************************
+ * *                         OOHOO - Tab Display                          **
+ * *************************************************************************
+ * @package     mod                                                       **
+ * @subpackage  tab                                                       **
+ * @name        tab                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
 
 namespace mod_tab\output;
 
