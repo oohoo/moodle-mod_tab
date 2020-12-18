d0b9de0745e6b72f9e04877daa26fc0832af46de
Mario Wehr <m.wehr@fh-kaernten.at>
2020-12-18 19:52:31 +0100

    Adds privacy support.

---
+++ classes/privacy/provider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * *************************************************************************
+ * *                         OOHOO - Tab Display                          **
+ * *************************************************************************
+ * @package     mod                                                       **
+ * @subpackage  tab                                                       **
+ * @name        tab                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+
+namespace mod_tab\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy Subsystem for local_yukaltura implementing null_provider.
+ */
+class provider implements
+    // This plugin does not store any personal user data.
+    \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return string - reason of why this plugin does not store users' data.
+     */
+    public static function _get_reason() {
+        return 'privacy:metadata';
+    }
+}
